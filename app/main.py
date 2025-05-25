20d594657414ce3420e7a934782d1e9e9d8b610a
examplefirstaccount <adagamov05@mail.ru>
2025-05-25 16:02:25 +0300

    include new routers

--- app/main.py
+++ app/main.py
@@ -1,3 +1,42 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from sqlalchemy import text
 
-app = FastAPI()
+from app.api.endpoints import users, tasks
+from app.core.config import settings
+from app.db.database import async_session_maker
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Startup code
+    try:
+        db = async_session_maker()
+        await db.execute(text("SELECT 1"))
+        await db.close()
+        print("✅ Database connected successfully.")
+    except Exception as e:
+        print("❌ Failed to connect to the database.")
+        raise e
+
+    yield
+
+    # Shutdown code
+    print("🛑 Application shutdown.")
+
+app = FastAPI(
+    title=settings.APP_NAME,
+    version=settings.APP_VERSION,
+    debug=settings.DEBUG,
+    lifespan=lifespan
+)
+app.include_router(users.router)
+app.include_router(tasks.router)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(
+        app="app.main:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=settings.DEBUG
+    )
